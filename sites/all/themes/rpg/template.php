3e42c06f3f4527edc2495ac56909d6461c0d134a
James Woods <rhinositedesign@vps709.getcadre.com>
2012-06-29 18:43:18 -0400

    Adding the height processing, including exceptionally short and tall.  Also added a theme function to display the height in 5'10" format

--- sites/all/themes/rpg/template.php
+++ sites/all/themes/rpg/template.php
@@ -10,3 +10,10 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+function rpg_field__field_height($variables) {
+  $height = $variables['element']['#items'][0]['value'];
+  $feet = floor($height/12);
+  $inches = $height%12;
+  $display = "$feet' $inches\"";
+  return $display;
+}
