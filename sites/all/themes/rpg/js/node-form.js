0c68685716d181f83cce5cb26a9004ca8f7f82e9
James Woods <rhinositedesign@vps709.getcadre.com>
2012-07-01 19:24:27 -0400

    Added the select all/deselect all jQuery functionality.

--- sites/all/themes/rpg/js/node-form.js
+++ sites/all/themes/rpg/js/node-form.js
@@ -1,8 +1,35 @@
-//console.log('I am included');
+(function ($) {
 
-Drupal.behaviors.rpgnodeform = {
-attach: function(context, settings) {
-//console.log('behaviors are calling')
+ Drupal.behaviors.rpgnpcnode = {
+attach: function (context) {
+console.log('still calling');
+
+$('.page-node-add .form-type-checkboxes .deselectall')
+.click(function() {
+  $(this)
+    .parent()
+    .next()
+    .find('input[type="checkbox"]')
+    .removeAttr('checked');
+  });
+
+
+$('.page-node-add .form-type-checkboxes .selectall')
+.click(function() {
+  $(this)
+    .parent()
+    .next()
+    .find('input[type="checkbox"]')
+    .attr('checked', true);
+  });
+
 }
+
+
+
 };
+
+
+
+})(jQuery);
 
